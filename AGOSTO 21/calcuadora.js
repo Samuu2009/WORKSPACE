e7baa37d5e1732a7843fbf590fc25d6978e41818
Samuu2009 <samuelbustamantec@ielacandelariamedellin.edu.co>
2025-08-21 16:31:57 -0500

    Add files via upload

---
+++ AGOSTO 21/calcuadora.js
@@ -0,0 +1,6 @@
+function suma(){
+    const elemento1 = document.getElementById("suma1");
+    const elemento2 = document.getElementById("suma2");
+    const resultado = document.getElementById("resultadosuma");
+    resultado.innerHTML = parseInt(elemento1.Value) + parseInt(elemento2.Value); 
+}
